a7678a066792e0b8a3a3f31c2a8c7e162d5ce0ce
kynas <45441764+kynas@users.noreply.github.com>
2020-06-03 19:43:56 +0100

    Update and rename scripts/sixpathdev_script.php to Gabrielstage2.py

---
+++ Gabrielstage2.py
@@ -0,0 +1,8 @@
+#stage 2 task
+
+my_name = "Kenneth Gabriel Tosin"
+my_ID = "HNG-05186"
+my_language = "Python"
+my_email = "gabrieltosin77@gmail.com"
+
+print("Hello World, this is " +my_name+ " with ID number "+my_ID+ " using " +my_language+" for stage 2 task")
